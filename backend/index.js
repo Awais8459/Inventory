e0bf3d145423646aa53c84a8ded9f1833ba6fe69
Awais8459 <67854951+Awais8459@users.noreply.github.com>
2023-06-08 18:20:03 +0500

    CRUD Operations

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const path = require('path');
+
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+
+
+const crudTask = require('./crud_task');
+app.use(crudTask);
+
+
+const port = 4000;
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://iamuser:iamuser@crudtask.7g1bgtp.mongodb.net/products?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log("MongoDB connection error: ", error);
+});
+
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Login from './login-jwt/login'
+import Crud from './crud_tasks/crud'
+
+function App() {
+  return (
+    <div>
+      <Crud/>
+    </div>
+  );
+}
+
+export default App;

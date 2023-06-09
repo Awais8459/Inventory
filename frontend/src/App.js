297964bff47758f40a7abaee8892d7b540afef10
Awais8459 <67854951+Awais8459@users.noreply.github.com>
2023-06-09 19:36:06 +0500

    React JS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Crud />} />
         <Route path="/uploadcsv" element={<UploadCSV />} />
